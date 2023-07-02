5a04881beeffeb723491fb61d8ecea0e9e0139d9
Andressa Paranhos <andressaparanhos1@hotmail.com>
2023-07-02 20:14:48 -0300

    feature(footer): created footer page

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        <div>
+            <p>
+                Dúvidas? Ligue <a :href="formattedPhoneNumber">{{ phoneNumber }}</a>
+            </p>
+            <div class="container">
+                <ul class="links-help">
+                    <li v-for= "link in links" :key="link.id">
+                        <a :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+  name: 'NxFooter',
+  data () {
+    return {
+      phoneNumber: '0800 591 8942'
+    }
+  },
+  props: {
+    links: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    formattedPhoneNumber () {
+      return `tel:${this.phoneNumber.replace(/\D/g, '')}`
+    }
+  }
+}
+</script>
+<style src="./styles.scss" scoped lang="scss"></style>
